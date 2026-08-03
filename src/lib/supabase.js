72ea28f18d159bd686f9309cd9a767ce91f9baaf
hanna1105 <154942664+hanna1105@users.noreply.github.com>
2026-08-03 11:32:32 +0900

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lxkzsigultcebssefdv.supabase.co'
+// 아래 주소 부분을 Data API에서 복사한 진짜 주소로 교체해 주세요!
+const supabaseUrl = 'https://lxkzsiguvltcebssefdv.supabase.co/rest/v1/'
 const supabaseAnonKey = 'sb_publishable_r02ELSGuwCwFhuRpPOj9MQ_CLjxmtk7'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
