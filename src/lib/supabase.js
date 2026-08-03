228b51a0acbf172f0a01148abd53559931cc92c4
hanna1105 <154942664+hanna1105@users.noreply.github.com>
2026-08-03 11:43:11 +0900

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // 끝에 /rest/v1 이 없는 순수 주소를 입력해야 합니다.
 const supabaseUrl = 'https://lxkzsigultcebssefdv.supabase.co'
-const supabaseAnonKey = 'sb_publishable_r02ELSGuwCwFhuRpPOj9MQ_CLjxmtk7'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4a3pzaWd1dmx0Y2Vic3NlZmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODkyMDQsImV4cCI6MjEwMTI2NTIwNH0.R2mR5pSgSsBijob2DlSOhYVCSfNYhmmZKUzuIyjDh0o'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
