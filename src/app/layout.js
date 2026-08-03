4583ad02254498668d8e8757aca1325054655e65
hanna1105 <154942664+hanna1105@users.noreply.github.com>
2026-08-03 11:18:24 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>{children}</body>
     </html>
   )
