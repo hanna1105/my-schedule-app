dd7acaf46d8ffbe1110050b0891dd39b56a983c0
hanna1105 <154942664+hanna1105@users.noreply.github.com>
2026-08-03 13:39:26 +0900

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lxkzsiguvltcebssefdv.supabase.co'
-
-// 아래에 Legacy anon 탭에서 복사한 eyJ... 로 시작하는 긴 키를 붙여넣으세요.
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4a3pzaWd1dmx0Y2Vic3NlZmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODkyMDQsImV4cCI6MjEwMTI2NTIwNH0.R2mR5pSgSsBijob2DlSOhYVCSfNYhmmZKUzuIyjDh0o.'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4a3pzaWd1dmx0Y2Vic3NlZmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODkyMDQsImV4cCI6MjEwMTI2NTIwNH0.R2mR5pSgSsBijob2DlSOhYVCSfNYhmmZKUzuIyjDh0o'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
